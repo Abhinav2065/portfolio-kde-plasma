c9766db45792532e1c1353708f5d6fa1c4d13db4
Abhinav2065 <abhinavsl511@gmail.com>
2025-11-21 20:58:15 +0545

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./loginScreen/Login";
+import { Link } from "react-router-dom";
+
+function App() {
+
+
+  return <div>
+    <Login/>
+  </div>
+}
+
+export default App;
+
---
+++ src/loginScreen/Login.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import './Login.css'
+import pfp from '../assets/pfp.png'
+import { Link } from 'react-router-dom'
+
+const Login = () => {
+
+  const today = new Date();
+  const dateString = today.toLocaleDateString();
+  const timeString = today.toLocaleTimeString('en-US', {
+    hour: '2-digit',
+    minute: '2-digit',
+    hours12: true 
+  });
+  const dayName = today.toLocaleDateString('en-US', { weekday: 'long' });
+
+  return (
+    <div>
+      <div className="header-text">
+        <h1>Hi! I am</h1>
+      </div>
+      <div className="pfp">
+        <Link to="/">
+          <img src={pfp} height="100vh" className='pfp'/>
+        </Link>
+      </div>
+      
+      <div className="name">
+        Abhinav
+      </div>
+      
+      <div className="info">
+        <div className="time">
+          {timeString}
+        </div>
+        <div className="date-day">
+          <div className="day">
+            {dayName}
+          </div>
+          <div className="date">
+            {dateString}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
