65f25bbdb10c2996bda2f575c8b28f8334033ed3
Abhinav2065 <abhinavsl511@gmail.com>
2025-11-22 18:22:24 +0545

    added the login screen thing such that the website redirects it to the login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter,Routes, Route, Navigate } from "react-router-dom";
 import Login from "./loginScreen/Login";
 import { Link } from "react-router-dom";
+import Desktop from "./Desktop/Desktop";
+
 
 function App() {
 
 
   return <div>
-    <Login/>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Navigate to='/login' replace />} />
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/desktop" element={<Desktop/>} />
+    </Routes>
+    </BrowserRouter>
   </div>
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,7 @@ import { Link } from "react-router-dom";
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BrowserRouter>
+  <StrictMode>  
     <App />
-    </BrowserRouter>
   </StrictMode>
 );
